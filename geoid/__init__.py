39cca2331efdae013a484077dac083049cdadc1f
Leideniya <84283107+Leideniya@users.noreply.github.com>
2023-07-03 16:36:22 +0700

    Deprecated old querier

--- geoid/__init__.py
+++ geoid/__init__.py
@@ -1,7 +1,6 @@
 __version__ = '2023.1'
 
 from geoid.bigquerier import BigQuerier
-from geoid.querier import Querier
 from geoid.query import Results
 from geoid.config import Config
 from geoid.logging import LOG_CONFIG
