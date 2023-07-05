0390d22dfcf588b960be4da106dee850639d06d1
Leideniya <84283107+Leideniya@users.noreply.github.com>
2023-07-05 15:39:54 +0700

    Small things

--- geoid/__init__.py
+++ geoid/__init__.py
@@ -3,7 +3,6 @@ __version__ = '2023.1'
 from geoid.bigquerier import BigQuerier
 from geoid.query import Results
 from geoid.config import Config
-from geoid.logging import LOG_CONFIG
 
 from geoid.query import get
 from geoid.main import begin
