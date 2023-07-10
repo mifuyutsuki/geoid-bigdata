6365f58d8413258b77c2fd3a6e88162d4aabe821
mifuyutsuki <84283107+mifuyutsuki@users.noreply.github.com>
2023-07-10 15:05:06 +0700

    Edit init and remove old bigquerier

--- geoid/__init__.py
+++ geoid/__init__.py
@@ -1,7 +1,10 @@
 __version__ = '0.1.0'
 
-from geoid.bigquerier import BigQuerier
-from geoid.query import Results
-from geoid.config import Config
-
-from geoid.query import get
+from . import bigquery
+from . import query
+from . import common
+from . import constants
+from . import processing
+from . import cli
+from . import config
+from . import logging
