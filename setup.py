10e6be716bd0a0e5d52605419c0f63c48805e04c
mifuyutsuki <84283107+mifuyutsuki@users.noreply.github.com>
2023-07-10 14:30:42 +0700

    Add setup

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 from geoid import __version__
 
 with open('README.md', encoding='UTF-8') as f:
@@ -12,7 +12,7 @@ install_requires = [
   'unidecode'
 ]
 
-#: Author and links pending publication
+#: Links pending publication
 
 setup(
   name='geoid-bigdata',
@@ -20,13 +20,18 @@ setup(
   description='Maps data scraper-parser for Indonesia places',
   long_description=readme,
   long_description_content_type='text/markdown',
-
-
+  author='mifuyutsuki',
   classifiers=[  
     "Programming Language :: Python :: 3.11"
   ],
   keywords='maps, scraper, indonesia',
   package_dir={'': 'geoid'},
+  packages=find_packages(where='geoid'),
   python_requires='>=3.11, <4',
-  install_requires=install_requires
+  install_requires=install_requires,
+  entry_points={
+    "console_scripts": [
+      "geoid=geoid.cli.cli:start"
+    ],
+  }
 )
