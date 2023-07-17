9cd1532394c1ec2df08a91c9e1a08ad279ccf3e3
mifuyutsuki <84283107+mifuyutsuki@users.noreply.github.com>
2023-07-17 14:12:00 +0700

    Changed showing of logs

--- geoid/logging.py
+++ geoid/logging.py
@@ -1,4 +1,4 @@
-import time
+import time, os, logging.config
 
 #: Logging configuration is set this way (as .py) due to the need to grab
 #: datetime information for the log file name.
@@ -10,31 +10,65 @@ LOG_FILENAME = r'logs\{0}.log'.format(LOG_DATETIME)
 #: Using DEBUG induces logging errors due to selenium logging the HTML, which
 #: contains special Unicode characters. (The program can continue, though.)
 
-LOG_CONFIG = {
-  "version": 1,
-  "disable_existing_loggers": False,
-  "formatters": {
-    "std": {
-      "format": LOG_FORMAT
+LOG_CONFIG_STD = {
+  'version': 1,
+  'disable_existing_loggers': False,
+  'formatters': {
+    'std': {
+      'format': LOG_FORMAT
     }
   },
-  "handlers": {
-    "console": {
-      "class": "logging.StreamHandler",
-      "stream": "ext://sys.stdout",
-      "level": "INFO",
-      "formatter": "std"
+  'handlers': {
+    'console': {
+      'class': 'logging.StreamHandler',
+      'stream': 'ext://sys.stdout',
+      'level': 'WARNING',
+      'formatter': 'std'
     },
-    "file": {
-      "class": "logging.FileHandler",
-      "filename": LOG_FILENAME,
-      "mode": "w",
-      "level": "INFO",
-      "formatter": "std"
+    'file': {
+      'class': 'logging.FileHandler',
+      'filename': LOG_FILENAME,
+      'mode': 'w',
+      'level': 'INFO',
+      'formatter': 'std'
     }
   },
-  "root": {
-    "level": "INFO",
-    "handlers": ["console", "file"]        
+  'root': {
+    'level': 'INFO',
+    'handlers': ['console', 'file']        
   }
 }
+
+
+LOG_CONFIG_INFO = LOG_CONFIG_STD.copy()
+LOG_CONFIG_INFO.update({
+  'handlers': {
+    'console': {
+      'level': 'INFO'
+    }
+  }
+})
+
+
+def _check_folder():
+  if not os.path.exists(r'./logs/'):
+    os.mkdir(r'./logs/')
+
+
+def log_std():
+  """
+  Start logging with level WARNING on stdout and INFO file.
+  """
+
+  _check_folder()
+  logging.config.dictConfig(LOG_CONFIG_STD)
+
+
+def log_info():
+  """
+  Start logging with level INFO on both stdout and file.
+  """
+
+  _check_folder()
+  logging.config.dictConfig(LOG_CONFIG_INFO)
+  
