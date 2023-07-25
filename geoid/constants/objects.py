52fbe1528515103e933c9b6c7f2d7a95434947db
mifuyutsuki <84283107+mifuyutsuki@users.noreply.github.com>
2023-07-25 15:53:23 +0700

    Move base place/result object to .constants

--- geoid/constants/objects.py
+++ geoid/constants/objects.py
@@ -12,3 +12,23 @@ class Objects:
     Keys.QUERY_RESULTS_COUNT : 0,
     Keys.QUERY_RESULTS       : []
   }
+
+  BASE_PLACE_OBJECT = {
+    Keys.LOCATION_NAME : None,
+    Keys.LOCATION_TYPE : None,
+    Keys.LATITUDE      : None,
+    Keys.LONGITUDE     : None,
+    Keys.PROVINCE_ID   : None,
+    Keys.PROVINCE_NAME : None,
+    Keys.CITY_ID       : None,
+    Keys.CITY_NAME     : None,
+    Keys.DISTRICT_ID   : None,
+    Keys.DISTRICT_NAME : None,
+    Keys.VILLAGE_ID    : None,
+    Keys.VILLAGE_NAME  : None,
+    Keys.POSTAL_CODE   : None,
+    Keys.RATING        : None,
+    Keys.REVIEWS       : None,
+    Keys.DESCRIPTION   : None,
+    Keys.LOCATION_LINK : None
+  }
