872aa5409bb58f14982897b7bd6b46a79500e998
mifuyutsuki <84283107+mifuyutsuki@users.noreply.github.com>
2023-07-26 15:29:00 +0700

    Remove unused .query.metadata

--- geoid/query/metadata.py
+++
@@ -1,8 +0,0 @@
-from geoid.constants import Status
-
-class Metadata:
-  def __init__(self):
-    self.status    = Status.QUERY_INCOMPLETE
-    self.query     = ''
-    self.lang      = ''
-    self.timestamp = 0
