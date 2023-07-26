11e6ddb299bcb336d521824554e76db5b46bbf9a
mifuyutsuki <84283107+mifuyutsuki@users.noreply.github.com>
2023-07-26 15:38:46 +0700

    Add copyright notice

--- geoid/logging.py
+++ geoid/logging.py
@@ -1,3 +1,23 @@
+# Copyright (c) 2023 mifuyutsuki
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 import time, os, logging.config
 
 #: Logging configuration is set this way (as .py) due to the need to grab
@@ -8,7 +28,7 @@ LOG_DATETIME = time.strftime('%Y%m%d_%H%M%S')
 LOG_FILENAME = r'logs\{0}.log'.format(LOG_DATETIME)
 
 #: Using DEBUG induces logging errors due to selenium logging the HTML, which
-#: contains special Unicode characters. (The program can continue, though.)
+#: may contain special Unicode characters. (The program can continue, though.)
 
 LOG_CONFIG_STD = {
   'version': 1,
